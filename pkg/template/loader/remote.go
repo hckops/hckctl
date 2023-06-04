0f53198de875b4f7f00da42334d76516abcd8fdf
niqdev <niqdev@users.noreply.github.com>
2023-06-04 22:03:23 +0100

    add refresh revision

--- pkg/template/loader/remote.go
+++ pkg/template/loader/remote.go
@@ -27,64 +27,12 @@ func NewRemoteTemplateLoader(opts *RemoteTemplateOpts) *RemoteTemplateLoader {
 	}
 }
 
-// TODO test checkout/pull with a repository
-// TODO do i need IsPathNotExist or i can just use clone error and revert "utils"
-
 // TODO list files https://gosamples.dev/list-files
 
 func (l *RemoteTemplateLoader) Load() (*TemplateValue, error) {
 
-	path := "/tmp/full/example/nested"
-	// c69656300c560af847e2e74da6d97a806cde2572
-	// main
-	// docker-0.4.9
-	revision := "main"
-
-	// first time clone repo always with default revision
-	// assume that path doesn't exist, or it's empty
-	if _, err := git.PlainClone(path, false, &git.CloneOptions{
-		URL:               "https://github.com/hckops/actions", // TODO l.opts.SourceUrl
-		RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
-		ReferenceName:     plumbing.NewBranchReferenceName(common.TemplateRevision),
-	}); err != nil && err != git.ErrRepositoryAlreadyExists {
-		return nil, errors.Wrap(err, "unable to clone repository")
-	}
-
-	// access repository
-	repository, err := git.PlainOpen(path)
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to open repository")
-	}
-	workTree, err := repository.Worktree()
-	if err != nil {
-		return nil, errors.Wrap(err, "unable to access repository")
-	}
-
-	if err := workTree.Pull(&git.PullOptions{}); err != nil && err != git.NoErrAlreadyUpToDate {
-		return nil, errors.Wrap(err, "unable to update revision")
-	}
-
-	// attempts checkout with the supported revision
-	checkoutStrategy := []*git.CheckoutOptions{
-		{Branch: plumbing.NewBranchReferenceName(revision)},
-		{Hash: plumbing.NewHash(revision)},
-		{Branch: plumbing.NewTagReferenceName(revision)},
-	}
-	var checkoutSuccess bool
-	for index, strategy := range checkoutStrategy {
-		if err := workTree.Checkout(strategy); err == nil {
-			log.Debug().Msgf("use checkout strategy %d", index)
-			checkoutSuccess = true
-		}
-	}
-	if !checkoutSuccess {
-		return nil, errors.Wrap(err, "unable to checkout revision")
-	}
-
-	if head, err := repository.Head(); err != nil {
-		return nil, errors.Wrap(err, "unable to verify revision")
-	} else {
-		log.Debug().Msgf("use revision revision=%s hash=%s", revision, head.Hash())
+	if err := l.refreshRevision(); err != nil {
+		return nil, errors.Wrap(err, "invalid revision")
 	}
 
 	// check if git source exists
@@ -95,4 +43,57 @@ func (l *RemoteTemplateLoader) Load() (*TemplateValue, error) {
 
 	localOpts := &LocalTemplateOpts{Path: "../megalopolis/boxes/official/alpine.yml", Format: l.opts.Format}
 	return NewLocalTemplateLoader(localOpts).Load()
+}
+
+func (l *RemoteTemplateLoader) refreshRevision() error {
+
+	// first time clone repo always with default revision
+	// assume that path doesn't exist, or it's empty
+	if _, err := git.PlainClone(l.opts.SourceCacheDir, false, &git.CloneOptions{
+		URL:               l.opts.SourceUrl,
+		RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
+		ReferenceName:     plumbing.NewBranchReferenceName(common.TemplateRevision),
+	}); err != nil && err != git.ErrRepositoryAlreadyExists {
+		return errors.Wrap(err, "unable to clone repository")
+	}
+
+	// access repository
+	repository, err := git.PlainOpen(l.opts.SourceCacheDir)
+	if err != nil {
+		return errors.Wrap(err, "unable to open repository")
+	}
+	workTree, err := repository.Worktree()
+	if err != nil {
+		return errors.Wrap(err, "unable to access repository")
+	}
+
+	// update current revision
+	if err := workTree.Pull(&git.PullOptions{}); err != nil && err != git.NoErrAlreadyUpToDate {
+		return errors.Wrap(err, "unable to update revision")
+	}
+
+	// attempts checkout for the latest revision
+	checkoutStrategy := []*git.CheckoutOptions{
+		{Branch: plumbing.NewBranchReferenceName(l.opts.Revision)},
+		{Hash: plumbing.NewHash(l.opts.Revision)},
+		{Branch: plumbing.NewTagReferenceName(l.opts.Revision)},
+	}
+	var checkoutSuccess bool
+	for index, strategy := range checkoutStrategy {
+		if err := workTree.Checkout(strategy); err == nil {
+			log.Debug().Msgf("use checkout strategy %d", index)
+			checkoutSuccess = true
+		}
+	}
+	if !checkoutSuccess {
+		return errors.Wrap(err, "unable to checkout revision")
+	}
+
+	if head, err := repository.Head(); err != nil {
+		return errors.Wrap(err, "unable to verify revision")
+	} else {
+		log.Debug().Msgf("use revision revision=%s hash=%s", l.opts.Revision, head.Hash())
+	}
+
+	return nil
 }
