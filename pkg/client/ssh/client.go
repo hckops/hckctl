2e97466b47c5fc28497c1753dc92598cc735d8b0
niqdev <niqdev@users.noreply.github.com>
2023-06-24 10:11:46 +0100

    update ssh client

--- pkg/client/ssh/client.go
+++ pkg/client/ssh/client.go
@@ -2,10 +2,15 @@ package ssh
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"net"
+	"os"
 
 	"github.com/pkg/errors"
-
 	gossh "golang.org/x/crypto/ssh"
+
+	"github.com/hckops/hckctl/pkg/client/common"
 )
 
 func NewSshClient(config *SshClientConfig) (*SshClient, error) {
@@ -22,6 +27,7 @@ func NewSshClient(config *SshClientConfig) (*SshClient, error) {
 	}, nil
 }
 
+// TODO ssh agent auth
 func sshClientConfig(config *SshClientConfig) *gossh.ClientConfig {
 	sshConfig := &gossh.ClientConfig{
 		User: config.Username,
@@ -40,7 +46,129 @@ func (client *SshClient) Close() error {
 func (client *SshClient) SendRequest(protocol string, payload string) (string, error) {
 	_, response, err := client.ssh.SendRequest(protocol, true, []byte(payload))
 	if err != nil {
-		return "", errors.Wrapf(err, "error send request")
+		return "", errors.Wrapf(err, "error ssh send request")
 	}
 	return string(response), nil
+}
+
+type TunnelOpts struct {
+	LocalPort             string
+	RemoteHost            string
+	RemotePort            string
+	OnTunnelErrorCallback func(error)
+}
+
+func (t *TunnelOpts) Network() string {
+	return "tcp"
+}
+
+func (t *TunnelOpts) LocalAddress() string {
+	return fmt.Sprintf("0.0.0.0:%s", t.LocalPort)
+}
+func (t *TunnelOpts) RemoteAddress() string {
+	return fmt.Sprintf("%s:%s", t.RemoteHost, t.RemotePort)
+}
+
+// Tunnel starts a local server and forwards traffic to a remote connection
+func (client *SshClient) Tunnel(opts *TunnelOpts) {
+
+	listener, err := net.Listen(opts.Network(), opts.LocalAddress())
+	if err != nil {
+		opts.OnTunnelErrorCallback(errors.Wrapf(err, "error ssh creating local tunnel: address=%s", opts.LocalAddress()))
+	}
+	defer listener.Close()
+
+	copyStream := func(writer, reader net.Conn, label string) {
+		defer writer.Close()
+		defer reader.Close()
+
+		_, err := io.Copy(writer, reader)
+		if err != nil {
+			opts.OnTunnelErrorCallback(errors.Wrapf(err, "error ssh copying stream: %s", label))
+		}
+	}
+
+	for {
+		localConnection, err := listener.Accept()
+		if err != nil {
+			opts.OnTunnelErrorCallback(errors.Wrapf(err, "error ssh opening local tunnel: address=%s", opts.LocalAddress()))
+		}
+		// forward connections
+		go func() {
+			remoteConnection, err := client.ssh.Dial(opts.Network(), opts.RemoteAddress())
+			if err != nil {
+				opts.OnTunnelErrorCallback(errors.Wrapf(err, "error ssh opening remote tunnel: address=%s", opts.RemoteAddress()))
+			}
+
+			go copyStream(localConnection, remoteConnection, "remote->local")
+			go copyStream(remoteConnection, localConnection, "local->remote")
+		}()
+	}
+}
+
+type ExecOpts struct {
+	Command               string
+	OnStreamStartCallback func()
+	OnStreamErrorCallback func(error)
+}
+
+func (client *SshClient) Exec(opts *ExecOpts) error {
+
+	session, err := client.ssh.NewSession()
+	if err != nil {
+		return errors.Wrapf(err, "error ssh new session")
+	}
+	defer session.Close()
+
+	if err := handleStreams(session, opts.OnStreamErrorCallback); err != nil {
+		return errors.Wrapf(err, "error ssh stream")
+	}
+
+	terminal, err := common.NewRawTerminal(os.Stdin)
+	if err != nil {
+		return errors.Wrap(err, "error ssh terminal")
+	}
+	defer terminal.Restore()
+
+	opts.OnStreamStartCallback()
+
+	if err := session.Run(opts.Command); err != nil && err != io.EOF {
+		return errors.Wrapf(err, "error ssh exec session")
+	}
+	return nil
+}
+
+func handleStreams(session *gossh.Session, onStreamErrorCallback func(error)) error {
+
+	stdin, err := session.StdinPipe()
+	if err != nil {
+		return errors.Wrap(err, "error opening stdin pipe")
+	}
+	go func() {
+		if _, err := io.Copy(stdin, os.Stdin); err != nil {
+			onStreamErrorCallback(errors.Wrap(err, "error copy stdin local->remote"))
+		}
+	}()
+
+	stdout, err := session.StdoutPipe()
+	if err != nil {
+		return errors.Wrap(err, "error opening stdout pipe")
+	}
+	go func() {
+		if _, err := io.Copy(os.Stdout, stdout); err != nil {
+			onStreamErrorCallback(errors.Wrap(err, "error copy stdout remote->local"))
+		}
+	}()
+
+	stderr, err := session.StderrPipe()
+	if err != nil {
+		return errors.Wrap(err, "error opening stderr pipe")
+	}
+	go func() {
+		if _, err := io.Copy(os.Stderr, stderr); err != nil {
+			onStreamErrorCallback(errors.Wrap(err, "error copy stderr remote->local"))
+		}
+	}()
+
+	return nil
 }
