5e06c5c071507b94a5737b376040263c3ac19412
niqdev <niqdev@users.noreply.github.com>
2023-08-02 06:58:56 +0100

    add common client test

--- pkg/client/common/util_test.go
+++ pkg/client/common/util_test.go
@@ -6,6 +6,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestDefaultShell(t *testing.T) {
+	assert.Equal(t, "/bin/bash", DefaultShell(""))
+	assert.Equal(t, "/bin/bash", DefaultShell("   "))
+	assert.Equal(t, "/bin/bash", DefaultShell("\n\r\t"))
+
+	assert.Equal(t, "foo", DefaultShell("foo"))
+}
+
 func TestToKebabCase(t *testing.T) {
 	assert.Equal(t, "hckops-my-test_value-example", ToKebabCase("hckops/my-test_value$example"))
 }
